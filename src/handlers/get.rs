ab942d860d4a589b72005c17d118238df78294b0
unknown <rrrr0ffff@gmail.com>
2023-09-02 01:11:54 -0300

    get all pokemons // not working yet

--- src/handlers/get.rs
+++ src/handlers/get.rs
@@ -1,3 +1,34 @@
-pub fn all_pokemons() -> String {
-    "all pokémon".to_string()
+use axum::{extract::State, Json};
+use serde::{Deserialize, Serialize};
+use sqlx::{error::Error, query_as, FromRow, SqlitePool};
+
+#[derive(Debug, Deserialize, FromRow, Serialize)]
+pub struct Pokemon {
+    pub id: i64,
+    pub name: String,
+    pub types: Vec<Type>,
+    pub height: i64,
+    pub weight: i64,
+    pub abilities: Vec<String>,
+    pub gif: String,
+    pub image: String,
+}
+
+#[derive(Debug, Deserialize, FromRow, Serialize)]
+pub struct Types {
+    pub slot: i64,
+    #[serde(rename = "type")]
+    pub type_field: Type,
+}
+
+#[derive(Debug, Deserialize, FromRow, Serialize)]
+pub struct Type {
+    pub name: String,
+    pub url: String,
+}
+
+pub async fn all_pokemons(State(pool): State<SqlitePool>) -> Result<Json<Vec<Pokemon>>, Error> {
+    let pokemons: Vec<Pokemon> = query_as("SELECT * FROM pokemon").fetch_all(&pool).await?;
+    println!("{:?}", pokemons);
+    todo!()
 }
