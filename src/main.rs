e7a286bc6f2e9ddde15a4885d83c1c947f1f79b0
unknown <rrrr0ffff@gmail.com>
2023-09-02 01:13:22 -0300

    await

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod handlers;
+mod infra;
 mod router;
 
 use router::router;
 
 #[tokio::main]
 async fn main() {
-    let app = router();
+    let app = router().await;
     axum::Server::bind(&"0.0.0.0:7878".parse().unwrap())
         .serve(app.into_make_service())
         .await
