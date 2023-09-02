3fd1a37d35296f8200dcacae15866a6443273fd2
unknown <rrrr0ffff@gmail.com>
2023-09-01 22:02:32 -0300

    hello, world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!".to_string() }));
+    axum::Server::bind(&"0.0.0.0:7878".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
 }
