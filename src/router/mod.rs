ef8ae3a3cc03ee81a7b64b03437724bc7b7ce09e
unknown <rrrr0ffff@gmail.com>
2023-09-02 01:15:55 -0300

    fixed handlers

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,9 +1,16 @@
 use axum::{routing::get, Router};
 
-use crate::handlers::{get::all_pokemons, index};
+use crate::{
+    handlers::{get::all_pokemons, index},
+    infra::database,
+};
 
-pub fn router() -> Router {
+pub async fn router() -> Router {
+    let connection = database::connect()
+        .await
+        .expect("Failed to connect to the database");
     Router::new()
-        .route("/", get(index()))
-        .route("/pokemon", get(all_pokemons()))
+        .route("/", get(index))
+        .route("/pokemon", get(all_pokemons))
+        .with_state(connection)
 }
