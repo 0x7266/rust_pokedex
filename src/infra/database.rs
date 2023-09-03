886ec3a76fbc7585ffde81fc125902ed36293528
unknown <rrrr0ffff@gmail.com>
2023-09-03 13:45:29 -0300

    fixed path

--- src/infra/database.rs
+++ src/infra/database.rs
@@ -1,8 +1,6 @@
 use sqlx::{Error, Pool, Sqlite, SqlitePool};
 
 pub async fn connect() -> Result<Pool<Sqlite>, Error> {
-    let url = std::env::var("DATABASE_URL")
-        .ok()
-        .unwrap_or("sqlite://database.sqlite".to_string());
+    let url = std::env::var("DATABASE_URL").unwrap_or("sqlite://sqlite.db".to_string());
     SqlitePool::connect(&url).await
 }
