b955fd59cc0083e8608f3c3141deecede7c01353
0x7266 <116040226+0x7266@users.noreply.github.com>
2024-06-03 00:15:57 -0300

    Update mod.rs

--- src/infra/http/server/mod.rs
+++ src/infra/http/server/mod.rs
@@ -2,7 +2,7 @@ use super::routes::router;
 
 pub async fn init() {
     let app = router().await;
-    let listener = tokio::net::TcpListener::bind("localhost:3333")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3333")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
